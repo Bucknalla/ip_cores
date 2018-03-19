dd2187aa488f6258bc31e0407030f5ad6ff00d98
bucknalla <alex.bucknall@gmail.com>
2018-03-19 17:34:12 +0000

    updated pilot ip

--- Pilot_Insertion_0.1/hdl/Pilot_Top.v
+++ Pilot_Insertion_0.1/hdl/Pilot_Top.v
@@ -21,6 +21,17 @@
 
 
 module Pilot_Top(
+    input clk,
+    input rst, // Active High
+    input [31:0] signal_in, 
+    input [4:0] frame_size,
+    input [3:0] pilot_interval,
+    output [31:0] signal_out,
+    output reg ready,
+    output reg valid,
+    output reg error
+);
 
-    );
+
+
 endmodule
